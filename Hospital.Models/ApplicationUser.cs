291989b444938f731d7b12321aba898d3121c924
Sara <malihatasnim530@gmail.com>
2025-09-02 00:41:52 +0600

    Adding models

--- Hospital.Models/ApplicationUser.cs
+++ Hospital.Models/ApplicationUser.cs
@@ -9,6 +9,27 @@ namespace Hospital.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public Gender MyProperty{get;set;}
+
+        public string Nationality { get; set; }
+
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+        public string Specialist { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
+
+        public ICollection<Payroll> Payrolls { get; set; }
+    }
+}
+
+namespace Hospital.Models
+{
+    public enum Gender
+    {
+        Male,Female,Other
     }
 }
 
