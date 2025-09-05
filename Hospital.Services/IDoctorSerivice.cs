8af16f631b6df6b9973531ed9b02d2acc1f9f535
zafrins <zafrin1412@gmail.com>
2025-09-06 00:14:21 +0600

    Doctor list1 plus admin ui

---
+++ Hospital.Services/IDoctorSerivice.cs
@@ -0,0 +1,21 @@
+﻿using Hospital.ViewModels;
+using Hospitals.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Services
+{
+    public interface IDoctorServices
+    {
+
+        PagedResult<TimingViewModel> GetAll(int pageNumber, int pageSize);
+        IEnumerable<TimingViewModel> GetAll();
+        TimingViewModel GetTimingById(int TimingId);
+        void UpdateTiming(TimingViewModel timing);
+        void AddTiming(TimingViewModel timing);
+        void DeleteTiming(int TimingId);
+    }
+}
---
+++ Hospital.ViewModels/TimingViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Hospital.Models;
+using Microsoft.AspNetCore.Mvc.Rendering; // for SelectListItem
+using System;
+using System.Collections.Generic;
+
+namespace Hospital.ViewModels
+{
+    public class TimingViewModel
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Doctor { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int MorningShiftStartTime { get; set; }
+        public int MorningShiftEndTime { get; set; }
+        public int AfternoonShiftStartTime { get; set; }
+        public int AfternoonShiftEndTime { get; set; }
+        public int Duration { get; set; } // in minutes
+
+        public Status Status { get; set; }
+
+        // Shift dropdown lists
+        public List<SelectListItem> MorningShiftStartOptions { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> MorningShiftEndOptions { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> AfternoonShiftStartOptions { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> AfternoonShiftEndOptions { get; set; } = new List<SelectListItem>();
+
+        public ApplicationUser DoctorId { get; set; }
+        public TimingViewModel() { }
+
+        public TimingViewModel(Timing model)
+        {
+            Id = model.Id;
+            Date = model.Date;
+            MorningShiftStartTime = model.MorningShiftStartTime;
+            MorningShiftEndTime = model.MorningShiftEndTime;
+            AfternoonShiftStartTime = model.AfternoonShiftStartTime;
+            AfternoonShiftEndTime = model.AfternoonShiftEndTime;
+            Duration = model.Duration;
+            Status = model.Status;
+            DoctorId = model.DoctorId;
+        }
+
+        public Timing ConvertViewModel(TimingViewModel model)
+        {
+            return new Timing
+            {
+                Id = model.Id,
+                Date = model.Date,
+                MorningShiftStartTime = model.MorningShiftStartTime,
+                MorningShiftEndTime = model.MorningShiftEndTime,
+                AfternoonShiftStartTime = model.AfternoonShiftStartTime,
+                AfternoonShiftEndTime = model.AfternoonShiftEndTime,
+                Duration = model.Duration,
+                Status = model.Status,
+                DoctorId = model.DoctorId,
+            };
+        }
+    }
+}
